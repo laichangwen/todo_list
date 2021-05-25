9d850f576b94d1118a666edf7adf64bffb517579
laichangwen <laichangwen21@gmail.com>
2021-05-26 01:28:16 +0800

    feat: add user login feature via passport

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require("../../models/user")
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-})
+router.post('/login',  passport.authenticate('local', 
+{ failureRedirect: '/users/login',
+  successRedirect: '/'}))
 
 router.get('/register', (req, res) => {
   res.render('register')
